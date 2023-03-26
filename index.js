a29648cd7d6294bfa461cff5709879d84caba323
Ianm234 <126569965+Ianm234@users.noreply.github.com>
2023-03-26 11:34:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const sinon = require('sinon');
+
+describe('index.js', () => {
+  let spy;
+
+  beforeEach(() => {
+    spy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    spy.restore();
+  });
+  function writeCards(namesArray, eventName) {
+  let messagesArray = [];
+
+  for (let i = 0; i < namesArray.length; i++) {
+    messagesArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return messagesArray;
+}
+
